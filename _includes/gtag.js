6fde9ca8c2ed11a5d71c8015b029698cd6d34921
Eduardo da Silva <emgsilva@gmail.com>
2023-05-08 23:00:20 +0200

    Migrate to GA4

---
+++ _includes/gtag.js
@@ -0,0 +1,10 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-JBKWZPQ4KP"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-JBKWZPQ4KP');
+  gtag('config', 'UA-114511801');
+</script>
